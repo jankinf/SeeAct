6d4d30cd824c4adedbecfe40a3b2bf5866bfc9bd
jankinf <fzw5782@126.com>
2025-02-27 16:42:11 +0800

    update dev openai test

---
+++ tests/dev/test_openai_new.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import re
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY", None),
+    base_url=os.getenv("OPENAI_BASE_URL", None),
+)
+system_prompt = "you're a helpful assistant"
+
+prompt_input = [
+    {"role": "system", "content": [{"type": "text", "text": system_prompt}]},
+    {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "tell me a joke"}
+        ],
+    },
+]
+
+response = client.chat.completions.create(
+    model="gpt-4o-2024-11-20",
+    messages=prompt_input,
+    max_tokens=50,
+    temperature=0,
+)
+
+answer = [choice.message.content for choice in response.choices][0]
+print(answer)
