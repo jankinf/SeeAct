811c5898128fe2f41571c9d17f2c435c06ac8621
jankinf <fzw5782@126.com>
2025-02-27 18:16:57 +0800

    new entry

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from seeact_package.seeact.agent import SeeActAgent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def run_agent():
+    agent = SeeActAgent(config_path='src/config/demo_mode.toml')
+    await agent.start()
+    while not agent.complete_flag:
+        prediction_dict = await agent.predict()
+        await agent.execute(prediction_dict)
+    await agent.stop()
+
+if __name__ == "__main__":
+    asyncio.run(run_agent())
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-PYTHONASYNCIODEBUG=1 python src/seeact.py
+# PYTHONASYNCIODEBUG=1 python src/seeact.py
+PYTHONASYNCIODEBUG=1 python main.py
