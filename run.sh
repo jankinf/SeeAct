c3d339b7206cd4582f1455c96adb8e88fe159386
jankinf <fzw5782@126.com>
2025-03-03 16:46:29 +0800

    update scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
+export HF_HUB_OFFLINE=1
+export LD_LIBRARY_PATH=/home/fangzhengwei/miniconda3/envs/seeact/lib/python3.10/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
+
 # PYTHONASYNCIODEBUG=1 python src/seeact.py
+# python -m tests.dev.test_inference_engine
+
 PYTHONASYNCIODEBUG=1 python main.py
--- tests/dev/rsync.sh
+++ tests/dev/rsync.sh
@@ -1,2 +1,11 @@
 # rsync -avcn --progress a100_jw:/home/chenjiawei/projects/seeact/* ./ --exclude={'__pycache__/',}
-rsync -av --progress a100_jw:/home/chenjiawei/projects/seeact/* ./ --exclude={'__pycache__/',}
+# rsync -av --progress a100_jw:/home/chenjiawei/projects/seeact/* ./ --exclude={'__pycache__/',}
+
+# rsync -av --progress ln01:/data/zhangyichi/cache/huggingface/hub/models--llava-hf--llava-1.5-7b-hf $HF_HOME/hub/
+# rsync -av --progress ln01:/data/zhangyichi/cache/huggingface/hub/models--meta-llama--Llama-3.2-11B-Vision-Instruct $HF_HOME/hub/
+# rsync -av --progress ln01:/data/zhangyichi/cache/huggingface/hub/models--llava-hf--llama3-llava-next-8b-hf $HF_HOME/hub/
+# rsync -av --progress ln01:/data/zhangyichi/cache/huggingface/hub/models--microsoft--Phi-3.5-vision-instruct $HF_HOME/hub/
+# rsync -av --progress ln01:/data/zhangyichi/cache/huggingface/hub/models--Qwen--Qwen2-VL-7B-Instruct $HF_HOME/hub/
+# rsync -av --progress ln01:/data/zhangyichi/cache/huggingface/hub/models--THUDM--glm-4v-9b $HF_HOME/hub/
+# rsync -av --progress ln01:/data/zhangyichi/cache/huggingface/hub/models--OpenGVLab--InternVL2-8B $HF_HOME/hub/
+rsync -av --progress ln01:/data/zhangyichi/cache/huggingface/hub/models--deepseek-ai--deepseek-vl2 $HF_HOME/hub/
